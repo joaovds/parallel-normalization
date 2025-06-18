817e87c3b560b9f54123a6e25a99ea09be195f19
João Victor da Silva <jv782063@gmail.com>
2025-06-18 10:17:11 -0300

    receiving settings by parameters

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/joaovds/parallel-normalization/normalization"
 )
 
 func main() {
+	inputFile := flag.String("i", "", "Path to input CSV file")
+	outputDir := flag.String("o", "./output", "Output directory")
+	sampleSize := flag.Int("samples", 2, "Number of samples to process (> 0)")
+	batchSize := flag.Int("batch", 4000, "Number of lines per batch (> 0")
+
+	flag.Parse()
+	validateParams(*inputFile, *outputDir, *sampleSize, *batchSize)
+
 	start := time.Now()
 
 	fileToNormalize := normalization.NewFileToNormalize("./small_dataset.csv", "./output", 8, 200000)
@@ -20,4 +30,24 @@ func main() {
 	elapsed := time.Since(start)
 	fmt.Println("Dataset normalized! :)")
 	fmt.Printf("Time: %s\n", elapsed)
+}
+
+func validateParams(inputFile, outputDir string, sampleSize, batchSize int) {
+	if inputFile == "" {
+		fmt.Println("Input file is required. Use -i <file>")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if sampleSize == 0 {
+		fmt.Println("Invalid sample size. Use sample size > 0")
+		flag.Usage()
+		os.Exit(1)
+	}
+
+	if batchSize == 0 {
+		fmt.Println("Invalid batch size. Use batch size > 0")
+		flag.Usage()
+		os.Exit(1)
+	}
 }
